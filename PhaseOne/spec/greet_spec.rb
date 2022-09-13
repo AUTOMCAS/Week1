0898cf4b8a28cd8b152926a6097c7e52a9163408
Chris <caston88@gmail.com>
2022-09-13 20:16:16 +0100

    PhaseOne complete

--- PhaseOne/spec/greet_spec.rb
+++ PhaseOne/spec/greet_spec.rb
@@ -1,6 +1,6 @@
 require "greet"
 
-RSpec.describe "greet" do
+RSpec.describe "greet method" do
   it "returns Hello name of user" do
     result = greet("Ruby")
     expect(result).to eq "Hello, Ruby!"

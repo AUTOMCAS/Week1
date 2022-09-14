0ad6e391ae346f8930d4a6514a1d168e5de0b797
Chris <caston88@gmail.com>
2022-09-14 12:05:27 +0100

    test to return original string if string is 5 words or less

--- PhaseTwo/lib/personal_diary_system.rb
+++ PhaseTwo/lib/personal_diary_system.rb
@@ -1,5 +1,6 @@
 # Method called make_snippet that takes a string as an argument and returns the first five words and then "..." if more than that.
 # Take a string and asign to variable and return it.
+# return original string if it is less than 5 words long
 # If string is more than 5 words long, 
   # return the first 5 words of string - split into array, join array but only first 5
   # add "..." to end of string.
@@ -7,6 +8,6 @@
 
 def make_snippet(str)
   arr = str.split()
-  return false if arr.length < 5
+  return str if arr.length < 5
   return "#{arr.first(5).join(" ")}..."
 end
--- PhaseTwo/spec/personal_diary_system_spec.rb
+++ PhaseTwo/spec/personal_diary_system_spec.rb
@@ -1,22 +1,15 @@
 require "personal_diary_system.rb"
 
 RSpec.describe "make_snippet method" do
-  it "stores and returns a given string" do
-    result = make_snippet("seal")
-    expect(result).to eq "seal..."
-  end
-
-  describe "make_snippet method" do
     it "returns first 5 words of given string, joined" do
       result = make_snippet("One Two Three Four Five Six")
       expect(result).to eq ("One Two Three Four Five...")
     end
-  end
   
   describe "make_snippet method" do
-    it "returns false if word is 5 or less words long" do
+    it "returns original string if word is 5 or less words long" do
       result = make_snippet("One two three")
-      expect(result).to eq (false)
+      expect(result).to eq ("One two three")
     end
   end
 

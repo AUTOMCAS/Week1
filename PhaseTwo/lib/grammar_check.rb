9afbe23e2e74b6bcbdc65f56bda07f9ef88d9c2f
Chris <caston88@gmail.com>
2022-09-14 19:12:05 +0100

    Tests complete

--- PhaseTwo/lib/grammar_check.rb
+++ PhaseTwo/lib/grammar_check.rb
@@ -1,4 +1,9 @@
 def grammar_check(text)
-  return "Grammar is good" if text[0] == text[0].upcase
-  return "Bad Grammar"
+  if text == "" || text == nil
+    fail "Error, empty"
+  elsif text[0] == text[0].upcase && text[-1].match(/[!.?]/)
+    "Grammar is good"
+  else
+    "Bad Grammar"
+  end
 end
